5dfc79113fd0686f8ff7da22859b9720f5669da9
Jeff Wong <jeffmjwong@gmail.com>
2019-08-30 19:21:10 +1000

    Print more statements

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
-
   public static void main(String[] args) {
-    System.out.println("Hello world!!");
+    System.out.println("Hello world one!");
+    System.out.println("Hello world two!");
+    System.out.println("Hello world three!");
   }
 }
+
