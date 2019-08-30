0299be0a2f0338d802dde16ba87df91de544ab07
Jeff Wong <jeffmjwong@gmail.com>
2019-08-30 19:22:19 +1000

    Add comment

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world one!");
+    System.out.println("Hello world one!"); // This is a comment
     System.out.println("Hello world two!");
     System.out.println("Hello world three!");
   }
