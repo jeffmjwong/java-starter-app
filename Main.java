ad40b641307c5f86e899ea052360defc4c51b7f5
Jeff Wong <jeffmjwong@gmail.com>
2019-08-30 18:20:18 +1000

    Log hello world string using system.out.println

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 public class Main {
 
-  public static String main(String[] args) {
-    var str1 = "hello world!";
-    // System.out.println(str1);
-    return str1;
+  public static void main(String[] args) {
+    System.out.println("Hello world!!");
   }
 }
