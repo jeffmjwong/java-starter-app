4006fdae72b367ef5415f648848206289014701e
Jeff Wong <jeffmjwong@gmail.com>
2019-08-31 19:55:49 +1000

    Use ++ and -- prefix and postfix operator

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,11 @@ public class Main {
 
     System.out.println(var1/var2);
 
+    float varPlus = ++var2;
+    System.out.println(varPlus);
+    varPlus--;
+    System.out.println(varPlus);
+
     BigDecimal var3 = new BigDecimal("2.15");
     BigDecimal var4 = new BigDecimal("1.1");
     System.out.println(var3.subtract(var4));
