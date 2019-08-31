33e443a2b3fb8b3c391036a21c8b310ac7fe395c
Jeff Wong <jeffmjwong@gmail.com>
2019-08-31 15:50:27 +1000

    Minus integer from float

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,8 @@ package src;
 
 public class Main {
   public static void main(String[] args) {
-    boolean isLoggedIn = true;
-    System.out.println(isLoggedIn);
-
-    int var1 = 5;
-    System.out.println(var1);
-
+    float var1 = 5.5f;
     int var2 = 10;
-    System.out.println(var2);
-
-    var1 = var2;
-    System.out.println(var1);
-    System.out.println(var2);
-
-    var2 = 20;
-    System.out.println(var1);
-    System.out.println(var2);
+    System.out.println(var2 - var1);
   }
 }
