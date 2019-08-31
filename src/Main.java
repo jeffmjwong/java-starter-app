5dd9a1cf4b87d370cc37e6949898e307bcddb024
Jeff Wong <jeffmjwong@gmail.com>
2019-08-31 21:28:38 +1000

    Cast operation between float type and short type back to short type

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,11 @@ public class Main {
   public static void main(String[] args) {
     long var1 = 13;
     short var2 = 5;
+    float var3 = 20.2f;
 
-    short var3 = (short) (var2 - var1);
+    short result1 = (short) (var3 - var1);
 
-    System.out.println(var3);
+    System.out.println(result1);
 
     // BigDecimal var3 = new BigDecimal("2.15");
     // BigDecimal var4 = new BigDecimal("1.1");
