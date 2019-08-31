3d404ea5e40fa7ff16c4088eba377f3fa65ecbfc
Jeff Wong <jeffmjwong@gmail.com>
2019-08-31 20:22:35 +1000

    Cast from long type to short type when doing arithmetic operation

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ package src;
 public class Main {
   public static void main(String[] args) {
     long var1 = 13;
+    short var2 = 5;
 
-    short var2 = (short) var1;
+    short var3 = (short) (var2 - var1);
 
-    System.out.println(var2);
+    System.out.println(var3);
 
     // BigDecimal var3 = new BigDecimal("2.15");
     // BigDecimal var4 = new BigDecimal("1.1");
