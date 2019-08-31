f66b8b45ef6a83fbe431753674a37e46043f265c
Jeff Wong <jeffmjwong@gmail.com>
2019-08-31 20:19:53 +1000

    Explicit cast long type to short type

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 package src;
 
-import java.math.BigDecimal;
-
 public class Main {
   public static void main(String[] args) {
-    short var1 = 13;
+    long var1 = 13;
 
-    long var2 = var1;
+    short var2 = (short) var1;
 
+    System.out.println(var2);
 
     // BigDecimal var3 = new BigDecimal("2.15");
     // BigDecimal var4 = new BigDecimal("1.1");
