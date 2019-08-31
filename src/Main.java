36d7b5e077d23ed60c0623ccfc3f34d5507d4edb
Jeff Wong <jeffmjwong@gmail.com>
2019-08-31 16:19:14 +1000

    Use BigDecimal to perform calculations that required exact accuracy

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 package src;
 
+import java.math.BigDecimal;
+
 public class Main {
   public static void main(String[] args) {
-    float var1 = 5.5f;
-    int var2 = 10;
-    System.out.println(var2 - var1);
+    float var1 = 2.15f;
+    float var2 = 1.1f;
+
+    BigDecimal var3 = new BigDecimal("2.15");
+    BigDecimal var4 = new BigDecimal("1.1");
+    System.out.println(var3.subtract(var4));
   }
 }
