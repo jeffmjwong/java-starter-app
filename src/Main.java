81b9d2896f2959b87efa10cee5a9672cd974a4e1
Jeff Wong <jeffmjwong@gmail.com>
2019-08-31 14:42:07 +1000

    Using the final keyword to assign const variable

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ package src;
 
 public class Main {
   public static void main(String[] args) {
-    int number1 = 5;
+    final int number1 = 5;
     System.out.println("Hello world one!"); // This is a comment
     System.out.println("Hello world two!");
     System.out.println(number1);
