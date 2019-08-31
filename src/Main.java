0f4bc3c85d60b342eb71f7954375945bd6c6b357
Jeff Wong <jeffmjwong@gmail.com>
2019-08-31 14:54:39 +1000

    Variables for int reference by value

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,13 @@ public class Main {
 
     int var2 = 10;
     System.out.println(var2);
+
+    var1 = var2;
+    System.out.println(var1);
+    System.out.println(var2);
+
+    var2 = 20;
+    System.out.println(var1);
+    System.out.println(var2);
   }
 }
