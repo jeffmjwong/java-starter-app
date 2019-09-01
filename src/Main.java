df20b9dd2339ae620f5916b475d5fd6862f0af7f
Jeff Wong <jeffmjwong@gmail.com>
2019-09-01 15:40:08 +1000

    Remove commented code

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,5 @@ public class Main {
     } else {
       System.out.println("hmm...");
     }
-
-    // BigDecimal var3 = new BigDecimal("2.15");
-    // BigDecimal var4 = new BigDecimal("1.1");
-    // System.out.println(var3.subtract(var4));
   }
 }
