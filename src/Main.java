4211ddf87f2d8868453dd104a3776fc48e2e4b48
Jeff Wong <jeffmjwong@gmail.com>
2019-09-01 14:37:41 +1000

    Use equal to operator

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,11 @@ package src;
 public class Main {
   public static void main(String[] args) {
     long var1 = 21;
-    short var2 = 5;
-    float var3 = 20.2f;
+    double var2 = 20.0;
+    float var3 = 20.0f;
 
     System.out.println(var1 > var3);
-
-    short result1 = (short) (var3 - var1);
-
-    System.out.println(result1);
+    System.out.println(var2 == var3);
 
     // BigDecimal var3 = new BigDecimal("2.15");
     // BigDecimal var4 = new BigDecimal("1.1");
